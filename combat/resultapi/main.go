40b600cb511de6425ee598a85bc630e567c01279
drzhangg <654014730@qq.com>
2018-12-26 13:24:23 +0800

    learngo

--- combat/resultapi/main.go
+++ combat/resultapi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
@@ -45,9 +46,23 @@ func PostPerson(w http.ResponseWriter, req *http.Request) {
 }
 
 func DeletePerson(w http.ResponseWriter, req *http.Request) {
-
+	params := mux.Vars(req)
+	for index,item := range people{
+		if item.ID == params["id"]{
+			people = append(people[:index],people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(people)
 }
 
 func main() {
-	
+	router := mux.NewRouter()
+	people = append(people,Person{ID:"1",Firstname:"xi",Lastname:"dada",Address:&Address{City:"hangzhou",Province:"zhejiang"}})
+	people = append(people,Person{ID:"2",Firstname:"li",Lastname:"xiansheng",Address:&Address{City:"zhengzhou",Province:"henan"}})
+	router.HandleFunc("/people",GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}",GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}",PostPerson).Methods("POST")
+	router.HandleFunc("/people/{id}",DeletePerson).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":12345",router))
 }
