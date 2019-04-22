633fb3a698d5def3b7ab9bc1c29c2b26c36bf453
drzhang <654014730@qq.com>
2019-04-22 10:44:00 +0800

    regex test

--- basic/regex/regex.go
+++ basic/regex/regex.go
@@ -5,16 +5,23 @@ import (
 	"regexp"
 )
 
-const text = `My email is ccmouse@gmail.com
-			email1 is abc@def.org
-			654014730@qq.com
+const text = `
+	my email is drzhangg@google.com
+	my1 email is drzhangg3@qq.com
+	my2 email is drzhangg4@def.com
+	my2 email is drzhangg4@def.com.cn
 `
 
 func main() {
 	re := regexp.MustCompile(`([a-zA-Z0-9]+)@([a-zA-Z0-9.]+)\.([a-zA-Z0-9]+)`)
 	match := re.FindAllStringSubmatch(text,-1)
-	fmt.Println(match)
-	for _,v := range match{
-		fmt.Println(v)
+	//fmt.Println(match)
+
+	var name []string
+	for _,m := range match{
+		name = append(name,m[1])
+		fmt.Println(m)
 	}
+	fmt.Println(name)
+
 }
