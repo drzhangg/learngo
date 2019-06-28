720949042cf2c84f186afd05ffe7d42d0a6e8a68
drzhang <654014730@qq.com>
2019-06-28 23:42:53 +0800

    一到面试题，有点死锁问题，明天继续解决

---
+++ channel/channel/main3.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func writeChan(numChan chan int) {
+	for i := 1; i <= 200; i++ {
+		numChan <- i
+	}
+	close(numChan)
+}
+
+//读取数据
+func resultChan(numChan chan int, resChan chan map[int]int, exitChan chan bool) {
+	m := make(map[int]int)
+	res := 0
+	for {
+		v, ok := <-numChan
+		if !ok {
+			break
+		} else {
+			//numChan每次进来是一个数，把进来的那个数存成key，和前面相加的和存成value
+			//v是每次进来取的数
+			res += v
+			m = map[int]int{v:res}
+			resChan <- m
+
+		}
+	}
+	exitChan <- true
+	close(exitChan)
+}
+
+func main() {
+
+	numChan := make(chan int, 200)
+	exitChan := make(chan bool, 1)
+	resChan := make(chan map[int]int, 200)
+
+	go writeChan(numChan)
+
+	go resultChan(numChan, resChan, exitChan)
+
+	for{
+		v,ok := <- resChan
+		if !ok {
+			break
+		}else {
+			fmt.Println(v)
+		}
+	}
+	defer close(resChan)
+
+
+
+	for {
+		_, ok := <-resChan
+		if !ok {
+			break
+		}
+	}
+
+
+}
