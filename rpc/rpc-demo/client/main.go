b8f0d055bfc4662957b8f793e6cc630e3920961d
drzhang <654014730@qq.com>
2019-07-17 22:43:05 +0800

    rpc demo

---
+++ rpc/rpc-demo/client/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+)
+
+func main() {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("rpc dial error:", err)
+	}
+
+	var reply string
+	err = client.Call("HelloService.Hello", "this is client", &reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(reply)
+}
