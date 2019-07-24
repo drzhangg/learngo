eb0c1af5a411d78e0a19de7903beaaa9e841e5f6
drzhang <654014730@qq.com>
2019-07-24 21:07:23 +0800

    写到一半访问不了网站的半成品

--- spider/main.go
+++ spider/main.go
@@ -14,25 +14,26 @@ type Doc struct {
 }
 
 func main() {
-
 	var (
 		docs []Doc
+		chapterList string
 	)
 
-	resp, err := http.Get("http://book.zongheng.com/store/c0/c0/b0/u0/p1/v9/s1/t0/u0/i1/ALL.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
+	url := "http://book.zongheng.com/store/c0/c0/b0/u0/p1/v9/s1/t0/u0/i1/ALL.html"
+	//url := "http://book.zongheng.com/book/860929.html"
+	urlResp := GetRespBody(url)
+	defer urlResp.Close()
 
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	doc, err := goquery.NewDocumentFromReader(urlResp)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(doc.Html())
 
 	doc.Find(".bookbox .bookname").Each(func(i int, s *goquery.Selection) {
 		bookname := s.Find("a").Text()
 		href, _ := s.Find("a").Attr("href")
+		fmt.Println(href)
 		docs1 := Doc{
 			BookName: bookname,
 			BookHref: href,
@@ -50,14 +51,35 @@ func main() {
 			log.Fatal("err:", err)
 		}
 		doc.Find(".btn-group").Each(func(i int, s *goquery.Selection) {
-			start := s.Find("a").Eq(1).Text()
+			//start := s.Find("a").Eq(1).Text()
 			href, _ := s.Find("a").Eq(1).Attr("href")
-			fmt.Println("start:", start)
-			fmt.Println("href:", href)
+			//fmt.Println("start:", d.BookName)
+			//fmt.Println("href:", href)
+
+			resb := GetRespBody(href)
+			defer resb.Close()
+			doc1,err := goquery.NewDocumentFromReader(resb)
+			if err != nil {
+				log.Fatal("err:", err)
+			}
+			doc1.Find(".chapter-list").Each(func(i int, s *goquery.Selection) {
+				chapterList ,_ = s.Find("li").Find("a").Attr("href")
+				//fmt.Println("chapterList:",chapterList)
+
+				contentHref := GetRespBody(chapterList)
+				defer contentHref.Close()
+				dd,err := goquery.NewDocumentFromReader(contentHref)
+				if err != nil {
+					log.Fatal("err:", err)
+				}
+				dd.Find("content").Each(func(i int, s *goquery.Selection) {
+					ss := s.Text()
+					fmt.Println("ss:",ss)
+				})
+
+			})
 		})
-
 	}
-
 }
 
 func GetRespBody(url string) io.ReadCloser {
