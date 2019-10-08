90d62a86faeb838b0d6c9c708568203e43c65b76
drzhang <654014730@qq.com>
2019-10-08 22:30:14 +0800

    grpc 认证

--- grpc-test/hello/client/main.go
+++ grpc-test/hello/client/main.go
@@ -3,16 +3,49 @@ package main
 import (
 	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"learngo/grpc-test/proto/hello"
 	"log"
 )
 
 const (
 	Address = "127.0.0.1:50052"
+
+	OpenTLS = true
 )
 
+type customerCredential struct {
+}
+
+func (c customerCredential) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appid":  "101010",
+		"appkey": "i am key",
+	}, nil
+}
+
+func (c customerCredential) RequireTransportSecurity() bool {
+	return OpenTLS
+}
+
 func main() {
-	conn, err := grpc.Dial(Address, grpc.WithInsecure())
+
+	var err error
+	var opts []grpc.DialOption
+
+	if OpenTLS {
+		creds, err := credentials.NewClientTLSFromFile("../learngo/grpc-test/hello/keys/server.pem", "zhang")
+		if err != nil {
+			log.Fatal(err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
+
+	opts = append(opts, grpc.WithPerRPCCredentials(new(customerCredential)))
+
+	conn, err := grpc.Dial(Address, opts...)
 	if err != nil {
 		log.Fatal(err)
 	}
--- grpc-test/hello/server/main.go
+++ grpc-test/hello/server/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 	pb "learngo/grpc-test/proto/hello"
 	"log"
 	"net"
@@ -19,8 +22,30 @@ type helloService struct {
 var HelloService = helloService{}
 
 func (h helloService) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, grpc.Errorf(codes.Unauthenticated, "无Token认证信息")
+	}
+
+	var (
+		appid  string
+		appkey string
+	)
+
+	if val, ok := md["appid"]; ok {
+		appid = val[0]
+	}
+	if val, ok := md["appkey"]; ok {
+		appkey = val[0]
+	}
+
+	if appid != "101010" || appkey != "i am key" {
+		return nil,grpc.Errorf(codes.Unauthenticated, "Token认证信息无效: appid=%s, appkey=%s", appid, appkey)
+	}
+	
 	resp := new(pb.HelloResponse)
-	resp.Message = fmt.Sprintf("Hello %s", in.Name)
+	resp.Message = fmt.Sprintf("Hello %s.\nToken info: appid=%s,appkey=%s", in.Name, appid, appkey)
 	return resp, nil
 }
 
@@ -31,10 +56,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	s := grpc.NewServer()
+	//TLS认证
+	creds, err := credentials.NewServerTLSFromFile("../learngo/grpc-test/hello/keys/server.pem", "../learngo/grpc-test/hello/keys/server.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//实例化grpc server，并开启TLS认证
+	s := grpc.NewServer(grpc.Creds(creds))
+	//注册helloService
 	pb.RegisterHelloServer(s, helloService{})
 
-	log.Println("Listen on " + Address)
+	log.Println("Listen on " + Address + " with TLS")
 
 	s.Serve(lis)
 
