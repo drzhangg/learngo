902780e8f6403985bc5918ea1c34f2942f01fa02
drzhang <654014730@qq.com>
2019-10-21 10:28:49 +0800

    learn go time

--- str/demo2/main.go
+++ str/demo2/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 func main() {
 	s := "30m10s"
 	ss := strings.Replace(s, "h", "小时", -1)
 	sss := strings.Replace(ss,"m","分钟",-1)
+
+	t := time.Unix(1571495639,0)
+	tt := t.Format("2006-01-02 15:04:05")
+	fmt.Println(tt)
+
+
 	fmt.Println(sss)
 }
