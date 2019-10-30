298f9e41bc7ae05be52cd7aa4ca5870ff8c880b7
drzhang <654014730@qq.com>
2019-10-30 23:38:45 +0800

    learn go yaml

---
+++ yml/demo1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Test struct {
+	Test1 int    `yaml:"Test1"`
+	Test2 string `yaml:"Test2"`
+}
+
+func main() {
+	var yamls Test
+	byes, err := ioutil.ReadFile("demo1.yaml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(byes))
+	err = yaml.Unmarshal(byes, &yamls)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(yamls)
+}
