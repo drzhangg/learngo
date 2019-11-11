406e124d0efff0e11c64d18c53e5a6b9269fb3f9
drzhang <654014730@qq.com>
2019-11-11 16:49:33 +0800

    learn go

--- string/demo1.go
+++ string/demo1.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"unicode/utf8"
+	"strconv"
 )
 
 func main() {
 
 	str := "430100000000"
-	utf8.RuneCountInString(str)
-	fmt.Println(str[5:6])
-	sss := strings.Replace(str, str[5:6], "1", )
-	fmt.Println(str)
-	fmt.Println(sss)
+	ss,_ := strconv.Atoi(str)
+	s := ss+999000
+	fmt.Println(strconv.Itoa(s))
+
+	fmt.Println(ss)
+	fmt.Println(s)
+	fmt.Println(ss + 999000)
 }
