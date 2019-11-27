0526b38a2622d85c05a9c627eda19d3fe3f9ee90
drzhang <654014730@qq.com>
2019-11-27 10:06:22 +0800

    rabbitmq test

--- rabbitmq/demo2/main.go
+++ rabbitmq/demo2/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/streadway/amqp"
 	"sync"
+	"time"
 )
 
 //定义q全局变量，指针类型
@@ -82,10 +83,15 @@ func New(q *QueueExchange) *RabbitMQ {
 
 //启动rabbitmq客户端，并初始化
 func (r *RabbitMQ) Start() {
-	//开启监听生产者发送任务
+	// 开启监听生产者发送任务
 	for _, producer := range r.producerList {
-		go
+		go r.listenProducer(producer)
 	}
+	// 开启监听接收者接收任务
+	for _, receiver := range r.receiverList {
+		go r.listenReceiver(receiver)
+	}
+	time.Sleep(1 * time.Second)
 }
 
 //注册发送指定队列指定路由的生产者
@@ -147,4 +153,64 @@ func (r *RabbitMQ) RegisterReceiver(receiver Receiver) {
 	r.mu.Lock()
 	r.receiverList = append(r.receiverList, receiver)
 	r.mu.Unlock()
+}
+
+//监听接收者接收任务
+func (r *RabbitMQ) listenReceiver(receiver Receiver) {
+	//处理结束关闭链接
+	defer r.mqClose()
+
+	//验证链接是否正常
+	if r.channel == nil {
+		r.mqConnect()
+	}
+
+	//用于检查队列是否存在，已经存在的话不需要重复声明
+	_, err := r.channel.QueueDeclarePassive(r.queueName, true, false, false, true, nil)
+	if err != nil {
+		// 队列不存在,声明队列
+		// name:队列名称;durable:是否持久化,队列存盘,true服务重启后信息不会丢失,影响性能;autoDelete:是否自动删除;noWait:是否非阻塞,
+		// true为是,不等待RMQ返回信息;args:参数,传nil即可;exclusive:是否设置排他
+		_, err = r.channel.QueueDeclare(r.queueName, true, false, false, true, nil)
+		if err != nil {
+			fmt.Printf("MQ注册队列失败:%s \n", err)
+			return
+		}
+	}
+
+	//绑定任务
+	err = r.channel.QueueBind(r.queueName, r.routingKey, r.exchangeName, true, nil)
+	if err != nil {
+		fmt.Printf("绑定队列失败:%s \n", err)
+		return
+	}
+
+	//获取消费通道，确保rabbitmq一个一个的发送消息
+	err = r.channel.Qos(1, 0, true)
+	msgList, err := r.channel.Consume(r.queueName, "", false, false, false, false,
+		nil)
+	if err != nil {
+		fmt.Printf("获取消费通道异常:%s \n", err)
+		return
+	}
+
+	for msg := range msgList {
+		// 处理数据
+		err := receiver.Consumer(msg.Body)
+		if err != nil {
+			err = msg.Ack(true)
+			if err != nil {
+				fmt.Printf("确认消息未完成异常:%s \n", err)
+				return
+			}
+		} else {
+			// 确认消息,必须为false
+			err = msg.Ack(false)
+			if err != nil {
+				fmt.Printf("确认消息完成异常:%s \n", err)
+				return
+			}
+			return
+		}
+	}
 }
